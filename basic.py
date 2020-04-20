3851b0d3f7110e6efe451dc036d268c371c87d31
Kayla Smith <50057385+smithkaylar@users.noreply.github.com>
2020-04-20 17:07:02 -0400

    explicitly defined templates folder
    Trying a solution from stack overflow to make heroku work, will revert if it doesn't work

--- basic.py
+++ basic.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template,session,redirect,url_for,request
 
 
-app =Flask(__name__)
+app =Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
