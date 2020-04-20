717de165193f6d50a1d09984080215e9062f6a6a
mittalj <59752236+mittalj@users.noreply.github.com>
2020-04-19 21:03:04 -0700

    Revert "Recipe pages added"

--- basic.py
+++ basic.py
@@ -7,12 +7,10 @@ app =Flask(__name__)
 def index():
     return render_template('index.html')
 
-#routes to food gallery
 @app.route('/entrees_gallery')
 def entrees_gallery():
     return render_template('entrees_gallery.html')
 
-#routes to 9 recipe pages
 @app.route('/roasted_bsprouts')
 def roasted_bsprouts():
     return render_template('roasted_bsprouts.html')
@@ -21,33 +19,6 @@ def roasted_bsprouts():
 def chicken_quesadila():
     return render_template('chicken_quesadila.html')
 
-@app.route('/thai_basil_chicken')
-def thai_basil_chicken():
-    return render_template('thai_basil_chicken.html')
-
-@app.route('/white_bean_soup')
-def white_bean_soup():
-    return render_template('white_bean_soup.html')
-
-@app.route('/italian_parmesean_chicken')
-def italian_parmesean_chicken():
-    return render_template('italian_parmesean_chicken.html')
-
-@app.route('/sweet_potato_chips')
-def sweet_potato_chips():
-    return render_template('sweet_potato_chips.html')
-
-@app.route('/green_beans')
-def green_beans():
-    return render_template('green_beans.html')
-
-@app.route('/orange_chicken')
-def orange_chicken():
-    return render_template('orange_chicken.html')
-
-@app.route('/lemon_rosemary_salmon')
-def lemon_rosemary_salmon():
-    return render_template('/lemon_rosemary_salmon.html')
 
 
 if __name__ == '__main__':
