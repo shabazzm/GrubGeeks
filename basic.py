58d6cb539a24c643ab4ed53ae9cf0b097e3a56eb
Kayla Smith <50057385+smithkaylar@users.noreply.github.com>
2020-05-17 14:55:53 -0400

    stuff and junk

--- basic.py
+++ basic.py
@@ -3,10 +3,23 @@ from flask import Flask, render_template,session,redirect,url_for,request, flash
 from flask_login import login_user,login_required,logout_user, current_user
 from myproject.dbModels import User_Accounts, Recipe_Calories, User_Posts, Post_Replies
 from myproject.forms import LoginForm,RegistrationForm, AddPostForm, AddReplyForm
+from myproject.forms import LoginForm,RegistrationForm, AddPostForm, AddReplyForm, CalorieCalcForm, AddRecipeForm
 from datetime import datetime
 from sqlalchemy.sql import select
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import desc
+from sqlalchemy import desc, between
+
+def add_to_db(item):
+	db.session.add(item)
+	db.session.commit()
+
+def execute_sql_get(query):
+	result = db.session.execute(query)
+	return result
+
+def execute_sql_post(query):
+	db.session.execute(query)
+	db.session.commit()
 
 #App Routes
 @app.route('/')
@@ -25,23 +38,34 @@ def logout():
 
 @app.route('/login',methods=['GET','POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User_Accounts.query.filter_by(email_address=form.email_address.data).first()
-        if user is None:
-            flash('This user does not exist in our system. Please try again.')
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User_Accounts.query.filter_by(email_address=form.email_address.data).first()
+		valid, error_msg = valid_login(user, form)
+		if valid:
+			login_user(user)
+			return get_redirect_page()
+		else:
+			flash(error_msg)
+	return render_template('login.html',form=form)
 
-        elif user.check_password(form.password.data) and user is not None:
-            login_user(user)
-            next = request.args.get('next')
+def valid_login(user, form):
+	error_msg = ""
+	valid = False
+	if user is None:
+		error_msg = "This user does not exist in our system. Please try again."
+	elif not user.check_password(form.password.data):
+		error_msg="Invalid username/password combination"
+	else:
+		valid = True
+	return valid, error_msg
 
-            if next == None or not next[0]=='/':
-                next = url_for('home')
+def get_redirect_page():
+	next = request.args.get('next')
+	if next == None or not next[0]=='/':
+		next = url_for('home')
+	return redirect(next)
 
-            return redirect(next)
-        else:
-            flash('Invalid username/password combination')
-    return render_template('login.html',form=form)
 
 @app.route('/register',methods=['GET','POST'])
 def register():
@@ -54,8 +78,7 @@ def register():
             user = User_Accounts(email_address=form.email_address.data,
                                 user_name=form.user_name.data,
                                 password=form.password.data)
-            db.session.add(user)
-            db.session.commit()
+            add_to_db(user)
             flash("You have successfully registered an account!")
             return redirect(url_for('login'))
 
@@ -82,8 +105,7 @@ def add_post():
         subject = form.subject.data
         main_post_content = form.main_post_content.data
         post = User_Posts(user_id=user_id, subject=subject, main_post_content=main_post_content, date_created=date_created)
-        db.session.add(post)
-        db.session.commit()
+        add_to_db(post)
         flash("Post Created")
         return redirect(url_for('forum'))
     else:
@@ -108,13 +130,13 @@ def post(post_id):
         reply_content = form.reply_content.data
         user_id=current_user.user_id
         date_created=datetime.now()
-        db.session.execute("INSERT INTO Post_Replies (post_id, user_id, reply_content, date_created) VALUES (" + str(post_id) + ", " + str(user_id) + ", '" + reply_content + "', '" +  str(date_created) + "')")
-        db.session.commit()
+        query="INSERT INTO Post_Replies (post_id, user_id, reply_content, date_created) VALUES (" + str(post_id) + ", " + str(user_id) + ", '" + reply_content + "', '" +  str(date_created) + "')"
+        execute_sql_post(query)
         flash("Reply Posted")
         return redirect('/post/' + str(post_id))
     else:
-        replies = db.session.execute("SELECT reply_id, user_id, reply_content, date_created FROM Post_Replies WHERE post_id = " + str(post_id))
-        return render_template('post.html', post=post, replies=replies, form=form, author=author)
+    	replies = execute_sql_get("SELECT reply_id, user_id, reply_content, date_created FROM Post_Replies WHERE post_id = " + str(post_id))
+    	return render_template('post.html', post=post, replies=replies, form=form, author=author)
 
 #routes to food gallery
 @app.route('/entrees_gallery')
@@ -122,10 +144,34 @@ def entrees_gallery():
     return render_template('entrees_gallery.html')
 
 #route to calorie calculator
-@app.route('/calorie_calc')
+@app.route('/calorie_calc', methods=['GET', 'POST'])
 def calorie_calc():
-    #Recipes = Recipe_Calories.query.all()
-    return render_template('calorie_calc.html')
+    form = CalorieCalcForm()
+    if form.validate_on_submit():
+        # do all the stuff
+        calories_in = int(form.daily_calories.data)
+        calorie_goal = int(form.calorie_goal.data)
+        if calories_in >= calorie_goal:
+            return redirect('')
+        else:
+        	optimal_calories = calorie_goal - calories_in
+        	return redirect('/calc_results/' + str(optimal_calories))
+    else:
+        return render_template('calorie_calc.html', form=form)
+
+@app.route('/calc_results/<int:optimal_calories>')
+def calc_results(optimal_calories):
+	calorie_low = optimal_calories-40
+	calorie_high = optimal_calories+40
+	query = "SELECT * from Recipe_Calories WHERE calories BETWEEN " + str(calorie_low) + " AND " + str(calorie_high)
+	recipes = db.session.execute(query)
+	# recipes = select(Recipe_Calories).where(between(Recipe_Calories.calories, calorie_low, calorie_high))
+	return render_template('all_db_recipes.html', recipes=recipes)
+
+@app.route('/recipe/<int:recipe_id>')
+def recipe(recipe_id):
+	recipe = Recipe_Calories.query.filter_by(recipe_id=recipe_id).first()
+	return render_template('recipe.html', recipe)
 
 #routes to 9 recipe pages
 @app.route('/roasted_bsprouts')
@@ -163,6 +209,23 @@ def orange_chicken():
 @app.route('/lemon_rosemary_salmon')
 def lemon_rosemary_salmon():
     return render_template('/lemon_rosemary_salmon.html')
+
+# temporary to add recipes to SQLite db
+@app.route('/add_recipe', methods=['GET', 'POST'])
+def add_recipe():
+	form=AddRecipeForm()
+	if request.method == 'POST':
+		recipe=Recipe_Calories(recipe_name=form.recipe_name.data, calories=form.calories.data)
+		db.session.add(recipe)
+		db.session.commit()
+		return redirect(url_for('add_recipe'))
+	else:
+		return render_template('add_recipe.html', form=form)
+
+@app.route('/all_db_recipes')
+def all_db_recipes():
+	recipes = Recipe_Calories.query
+	return render_template('all_db_recipes.html', recipes=recipes)
 
 @app.errorhandler(404)
 def page_not_found(e):
