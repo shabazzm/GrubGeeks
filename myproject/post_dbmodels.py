839698e58a0a4912ed8074d4842c93f4f0e7f9a2
barwickl <barwickl@oregonstate.edu>
2020-05-23 18:34:45 -0700

    modified init position and rechecked indentations

--- myproject/post_dbmodels.py
+++ myproject/post_dbmodels.py
@@ -6,26 +6,20 @@ from flask_login import UserMixin
 
 # Base Post
 class Post(db.Model):
+
+    post_id = db.Column('post_id', db.Integer, primary_key=True, nullable=False)    
+    user_id = db.Column(db.Integer, db.ForeignKey('user_accounts.user_id'), nullable=False)    
+    date_created = db.Column('date_created', db.DateTime, nullable=False)
 	
 	def __init__(self, user_id, post_id, date_created):
 	    self.user_id = user_id
 	    self.post_id = post_id
 	    self.date_created = date_created
-
-    post_id = db.Column('post_id', db.Integer, primary_key=True, nullable=False)
     
-    user_id = db.Column(db.Integer, db.ForeignKey('user_accounts.user_id'), nullable=False)
-
-    date_created = db.Column('date_created', db.DateTime, nullable=False)
 
 
 #User Posts
 class Topic(Post):
-
-    def __init__(self, user_id, post_id, subject, main_post_content, date_created):
-    	super().__init__(user_id=user_id, post_id=post_id, date_created=date_created)
-        self.subject = subject
-        self.main_post_content = main_post_content
 
     __tablename__ = 'user_posts'
     __table_args__ = {'sqlite_autoincrement': True}
@@ -36,17 +30,22 @@ class Topic(Post):
     
     post_replies = db.relationship('Post_Replies', backref='user_posts',lazy='dynamic')
 
+    def __init__(self, user_id, post_id, subject, main_post_content, date_created):
+        super().__init__(user_id=user_id, post_id=post_id, date_created=date_created)
+        self.subject = subject
+        self.main_post_content = main_post_content
+
 
 #User Replies
 class Reply(Post):
 
-	def __init__(self, user_id, post_id, subject, main_post_content, date_created):
-	    	super().__init__(user_id=user_id, post_id=post_id, date_created=date_created)
-	        self.reply_content = reply_content
-
-    __tablename__ = 'post_replies'
+	__tablename__ = 'post_replies'
     __table_args__ = {'sqlite_autoincrement': True}
     
     reply_id = db.Column('reply_id', db.Integer, primary_key=True)
 
     reply_content = db.Column('subject', db.String(254), nullable=False)
+
+    def __init__(self, user_id, post_id, subject, main_post_content, date_created):
+            super().__init__(user_id=user_id, post_id=post_id, date_created=date_created)
+            self.reply_content = reply_content
